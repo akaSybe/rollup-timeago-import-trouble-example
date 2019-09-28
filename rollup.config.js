502fbbae3cef8990a03a60f76c3826f76fd0c4dc
Aleksandr Shestakov <akasybe@gmail.com>
2019-09-29 00:06:17 +0300

    Cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -9,8 +9,8 @@ export default [
 			format: 'iife'
 		},
 		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
+			resolve(),
+			commonjs()
 		]
 	}
 ];
